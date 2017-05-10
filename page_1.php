9910f8310a9390dd44e93b32f8df0d37f7e3bac7
MarielaCamposs <micampos1@uc.cl>
2017-05-10 12:59:54 -0300

    *

---
+++ page_1.php
@@ -0,0 +1,8 @@
+<?php include('header.php');?>
+<?php $aqui = "Lo intente :C "?>
+<div class="inner cover">
+<h2 class="cover-heading">numeros de zonas wifi</h2>
+<p class="lead"><?php echo $aqui;?></p>
+<div id="top"></div>
+</div>
+<?php include('footer.php');?>
